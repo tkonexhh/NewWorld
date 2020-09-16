eb0d5373e3555133fa90b25cf1cbbc0ba3a945f5
neo.xu <410830616@qq.com>
2020-09-16 17:55:06 +0800

    add map sloation

---
+++ UnityProject/NewWorld/Assets/Scripts/Game/Module/OpenWorld/MightyTerrainMesh/Scripts/MTUtilities.cs
@@ -0,0 +1,38 @@
+﻿namespace MightyTerrainMesh
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+
+    //array
+    public class MTArray<T>
+    {
+        public T[] Data;
+        public int Length { get; private set; }
+        public MTArray(int len)
+        {
+            Reallocate(len);
+        }
+        public void Reallocate(int len)
+        {
+            if (Data != null && len < Data.Length)
+                return;
+            Data = new T[len];
+            Length = 0;
+        }
+        public void Reset()
+        {
+            Length = 0;
+        }
+        public void Add(T item)
+        {
+            if (Data == null || Length >= Data.Length)
+            {
+                Debug.LogError("MTArray overflow : " + typeof(T));
+            }
+            Data[Length] = item;
+            ++Length;
+        }
+    }
+}
