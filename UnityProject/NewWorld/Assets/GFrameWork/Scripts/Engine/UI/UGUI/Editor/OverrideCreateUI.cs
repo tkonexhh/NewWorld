d41765184ee12d1fc617b873a89f3f7399849cc0
neo.xu <410830616@qq.com>
2021-04-26 13:55:58 +0800

    添加一些编辑器小工具

--- UnityProject/NewWorld/Assets/GFrameWork/Scripts/Engine/UI/UGUI/Editor/OverrideCreateUI.cs
+++ UnityProject/NewWorld/Assets/GFrameWork/Scripts/Engine/UI/UGUI/Editor/OverrideCreateUI.cs
@@ -67,22 +67,25 @@ namespace GFrame.Editor
         private static void CreateUI(System.Func<GameObject> callback)
         {
             var canvasObj = SecurityCheck();
-
+            var gameObject = callback();
             if (!Selection.activeTransform)      // 在根目录创建的， 自动移动到 Canvas下
             {
-                callback().transform.SetParent(canvasObj.transform);
+                gameObject.transform.SetParent(canvasObj.transform);
             }
             else // (Selection.activeTransform)
             {
                 if (!Selection.activeTransform.GetComponentInParent<Canvas>())    // 没有在UI树下
                 {
-                    callback().transform.SetParent(canvasObj.transform);
+                    gameObject.transform.SetParent(canvasObj.transform);
                 }
                 else
                 {
-                    callback();
+                    //callback();
                 }
             }
+
+            Undo.IncrementCurrentGroup();
+            Undo.RegisterCreatedObjectUndo(gameObject, "");
         }
 
         #region Create
--- UnityProject/NewWorld/Assets/GFrameWork/Tools/Editor/PathTools.cs
+++ UnityProject/NewWorld/Assets/GFrameWork/Tools/Editor/PathTools.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Diagnostics;
 using UnityEngine;
 using UnityEditor;
 
@@ -11,14 +11,49 @@ namespace GFrame.Editor
         [MenuItem("Tools/Path/Open Asset Path")]
         private static void OpenAssetPath()
         {
-            Application.OpenURL("file:///" + Application.dataPath);
+            Execute(Application.dataPath);
         }
 
         [MenuItem("Tools/Path/Open Peristent Path")]
         private static void OpenPeristentPath()
         {
-            Debug.LogError(Application.persistentDataPath);
-            Application.OpenURL("file:///" + Application.persistentDataPath);
+            Execute(Application.persistentDataPath);
+        }
+
+        [MenuItem("Tools/Path/Open StreamingAssets Path")]
+        private static void OpenStreamingAssetsPath()
+        {
+            Execute(Application.streamingAssetsPath);
+        }
+
+        [MenuItem("Tools/Path/Open TemporaryCachePath Path")]
+        private static void OpenTemporaryCachePathPath()
+        {
+            Execute(Application.temporaryCachePath);
+        }
+
+
+        /// <summary>
+        /// 打开指定路径的文件夹。
+        /// </summary>
+        /// <param name="folder">要打开的文件夹的路径。</param>
+        public static void Execute(string folder)
+        {
+            folder = string.Format("\"{0}\"", folder);
+            switch (Application.platform)
+            {
+                case RuntimePlatform.WindowsEditor:
+                    Process.Start("Explorer.exe", folder.Replace('/', '\\'));
+                    break;
+
+                case RuntimePlatform.OSXEditor:
+                    Process.Start("open", folder);
+                    break;
+
+                default:
+                    throw new Exception(string.Format("Not support open folder on '{0}' platform.",
+                        Application.platform.ToString()));
+            }
         }
     }
 }
